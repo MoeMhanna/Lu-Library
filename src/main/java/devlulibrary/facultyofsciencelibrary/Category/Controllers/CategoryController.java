edc3eaeb15c911dd5f35e5f77fc6faf65eaaee29
Hamoud Mhanna <hamoud@alchemi-data.com>
2023-12-10 14:11:21 -0500

    Added: code refactor

--- src/main/java/devlulibrary/facultyofsciencelibrary/Category/Controllers/CategoryController.java
+++ src/main/java/devlulibrary/facultyofsciencelibrary/Category/Controllers/CategoryController.java
@@ -1,4 +1,5 @@
 package devlulibrary.facultyofsciencelibrary.Category.Controllers;
+
 import devlulibrary.facultyofsciencelibrary.Books.Dto.BooksForResponseDto;
 import devlulibrary.facultyofsciencelibrary.Category.Dto.CategoryForCreationDto;
 import devlulibrary.facultyofsciencelibrary.Category.Model.CategoryModel;
@@ -13,27 +14,33 @@ import java.util.List;
 @RequestMapping("/Category")
 public class CategoryController {
     private final CategoryService categoryService;
-    CategoryController(CategoryService categoryService){
+
+    CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
+
     @GetMapping
-    public ResponseEntity<List<CategoryModel>> listCategory(){
+    public ResponseEntity<List<CategoryModel>> listCategory() {
         return this.categoryService.getCategoryList();
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<CategoryModel> getCategoryById(@PathVariable String id){
+    public ResponseEntity<CategoryModel> getCategoryById(@PathVariable String id) {
         return this.categoryService.getCategoryById(id);
     }
+
     @GetMapping("/{categoryName}/books")
-    public ResponseEntity<List<BooksForResponseDto>> getCategoryBooks(@PathVariable String categoryName){
+    public ResponseEntity<List<BooksForResponseDto>> getCategoryBooks(@PathVariable String categoryName) {
         return this.categoryService.getCategoryBooks(categoryName);
     }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<CategoryModel> deleteCategory(String id){
+    public ResponseEntity<CategoryModel> deleteCategory(@PathVariable String id) {
         return this.categoryService.deleteCategory(id);
     }
+
     @PostMapping
-    public ResponseEntity<CategoryModel> addCategory(@RequestBody CategoryForCreationDto category){
+    public ResponseEntity<CategoryModel> addCategory(@RequestBody CategoryForCreationDto category) {
         return this.categoryService.addCategory(category);
     }
 }
